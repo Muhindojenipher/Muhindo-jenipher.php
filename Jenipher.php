0ef8bb9edf077cd0d2c657c2cb38f02922135e88
Muhindojenipher <mutokajennipher@gmail.com>
2024-09-20 18:30:38 +0300

    Initial commit

---
+++ Jenipher.php
@@ -0,0 +1,16 @@
+<?php 
+$Name= "MUHINDO JENIPHER";
+$DOB = "27/07/2000";
+$Address = "BWERA-KASESE";
+$current_year = 2024;
+$year_of_birth = 2000;
+$age = $current_year - $year_of_birth;
+$welcome = "WELL BE BACK  HOME";
+
+echo "Name: " . $Name . "\n";
+echo "Date of birth: " . $DOB . "\n";
+echo "Home Address: " . $Address . "\n";
+echo "Welcome! JENIPHER, you are now " . $age . " years old.\n";
+echo "DEAR" . $welcome . "\n";
+
+?>
